76ccbf27b434db37c532274df7067d55c19fc7eb
unknown <kcavner@hotmail.com>
2023-08-16 15:39:35 -0500

    login screen styled

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(fas);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
+import {Link} from 'react-router-dom'
 import Auth from '../utils/auth'
 import axiosInstance from '../utils/axiosInstance';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBolt } from '@fortawesome/free-solid-svg-icons';
 
 export default function Login(){
     const [username,setUsername] = useState();
@@ -28,22 +31,28 @@ export default function Login(){
     }
 
     return(
-        <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
+        <div className='login-container'>
+      
+      <form className="login-form" onSubmit={handleLogin}>
+      <h2><span className='ez-span'>EZ</span>tiers</h2>
+      <p>welcome to EZtiers! <FontAwesomeIcon style={{color: "ffea00",}} icon={faBolt} /> </p>
         <input
+        className='login-item'
           type="text"
           placeholder="Username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
         <input
+        className='login-item'
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Login</button>
+        <button className='login-button' type="submit">Login</button>
+        <p>Not a user?</p>
+        <Link className="create-account" to="/register">Create an account!</Link>
       </form>
     </div>
     )
